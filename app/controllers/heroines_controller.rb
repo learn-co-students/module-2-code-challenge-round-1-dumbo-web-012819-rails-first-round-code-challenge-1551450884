6aa1bb7e25a5787f1933ff61dcf62bcd1794f34e
github username <github email address>
2019-03-01 11:10:44 -0500

    I guess I'm doing the project by myself

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,40 @@
 class HeroinesController < ApplicationController
+  
+  before_action :get_heroines, only: [:show, :update, :create]
+
   def index
-    @heroines = Heroine.all
+  	@heroines = Heroine.all
+  end
+
+  def show
+  	
+  end
+
+  def new
+  	@heroine = Heroine.new
+  end
+
+  def create
+  	@heroine = Heroine.create(heroine_params)
+
+  	redirect_to heroine_path(@heroine)
+    # if @heroine.valid?
+      
+    # else
+    #   render :new
+    # end
+  end
+
+
+  private
+
+  def heroine_params
+	params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
+  def get_heroines
+  	# byebug
+  	@heroine = Heroine.find(params[:id])
+
   end
 end
