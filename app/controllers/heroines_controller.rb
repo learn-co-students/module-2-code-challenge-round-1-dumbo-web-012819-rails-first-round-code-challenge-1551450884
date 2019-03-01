99c22c3fb64bd14e9c2956cd9900c2922bf0a1da
Tracyaa <tracyho0867@gmail.com>
2019-03-01 11:10:33 -0500

    tracy mod2 challenge complete

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,48 @@
 class HeroinesController < ApplicationController
+
+  before_action :get_heroine, only: [:show, :edit, :update]
+
   def index
     @heroines = Heroine.all
   end
+
+  def show
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.create(heroine_params)
+    if @heroine.valid?
+      redirect_to @heroine
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @heroine.update(heroine_params)
+    if @heroine.valid?
+      redirect_to @heroine
+    else
+      redirect_to @heroine
+    end
+  end
+
+
+  private
+
+  def get_heroine
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
 end
