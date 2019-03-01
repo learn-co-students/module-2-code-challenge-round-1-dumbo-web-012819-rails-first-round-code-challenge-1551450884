e7197224167c4cd3d4d54a1632b15dd2724b2735
Cristian Leiva <cleiva94.cl@gmail.com>
2019-03-01 11:08:09 -0500

    finished except for bonus

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,45 @@
 class HeroinesController < ApplicationController
+  before_action :get_heroine, only: [:show, :edit, :update]
   def index
-    @heroines = Heroine.all
+    @heroines = if params[:true]
+      Heroine.where('name LIKE ?', "%#{params[:heroine]}%")
+    else
+      Heroine.all
+    end
+  end
+
+  def show
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.create(heroine_params)
+    if @heroine.valid?
+      redirect_to @heroine      
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @heroine.update(heroine_params)
+      redirect_to @heroine
+    else
+      render :edit
+    end
+  end
+  private
+  def get_heroine
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id, :heroine)
   end
 end
--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,6 @@
 class Heroine < ApplicationRecord
+  belongs_to :power
+  validates :name, :super_name, presence: true
+  validates :name, :super_name, uniqueness: true
+  # validates :self, uniqueness: {scope: :power, message: "You can only have one power!!!"}
 end
