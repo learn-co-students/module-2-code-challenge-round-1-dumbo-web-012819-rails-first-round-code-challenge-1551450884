0dcf82bf0c1cf9128c0e3eeab58e29ba03958b58
Maximo Bautista <max.bautista97@gmail.com>
2019-03-01 11:13:10 -0500

    Done

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,61 @@
 class HeroinesController < ApplicationController
+
+  before_action :get_heroine, only: [:show]
+
   def index
+    @search_off = true
     @heroines = Heroine.all
   end
+
+  def search
+    if params[:q] != nil
+      @heroines = Heroine.with_super_power(params[:q])
+      if @heroines.count > 0
+        @hero_search = "Heros found with that power"
+      else
+        @hero_search = "No heros found with that power."
+      end
+      @search_off = false
+    else
+      @search_off = true
+      @heroines = Heroine.all
+    end
+    render :index
+  end
+
+  def show
+  end
+
+  def new
+    @heroine = Heroine.new
+    common_requirements
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.valid?
+      @heroine.save
+      redirect_to @heroine
+    else
+      common_requirements
+      render :new
+    end
+  end
+
+
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
+  def get_heroine
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def common_requirements
+    @powers = Power.all
+  end
+
 end
--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,16 @@
 class Heroine < ApplicationRecord
+  belongs_to :power
+  validates :name, presence: true
+  validates :super_name, presence: true
+  validates :super_name, uniqueness: true
+  validates :power_id, presence: {message: 'must be selected'}
+
+  def self.with_super_power(power)
+
+    Heroine.all.select do |hero|
+        hero.power.name.downcase == power.downcase
+    end
+
+  end
+
 end
