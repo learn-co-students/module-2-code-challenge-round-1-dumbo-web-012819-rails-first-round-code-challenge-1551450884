1d50752719e73087ae393272be7e0eb6a66e3d7a
github username <github email address>
2019-03-01 11:10:24 -0500

    created the super heroine app with a heroine and a power models.

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,39 @@
 class HeroinesController < ApplicationController
+  before_action :get_heroine, only: [:show]
+
   def index
-    @heroines = Heroine.all
+    if params[:q]
+      @heroines = Heroine.filter_heroines(params[:q])
+    else
+      @heroines = Heroine.all
+    end
+  end
+
+  def show
+  end
+
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all
+  end
+
+  def create
+    @heroine= Heroine.create(heroine_params)
+    @powers = Power.all
+    if @heroine.valid?
+      redirect_to(heroine_path(@heroine))
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
+  def get_heroine
+    @heroine = Heroine.find(params[:id])
   end
 end
