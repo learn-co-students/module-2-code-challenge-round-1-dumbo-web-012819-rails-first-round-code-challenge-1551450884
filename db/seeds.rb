1f66e07bed6796be6f170b75eba2e8c588563503
Nate <github email address>
2019-03-01 11:12:23 -0500

    code challenge challenged

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,20 +7,20 @@ powers = [
   {name:"super human senses", description: "allows the wielder to user her senses at a super-human level" },
   {name:"elasticity", description: "can stretch the human body to extreme lengths" }
 ]
-
+ 
 powers.each {|power| Power.create(power)}
 
 heroines = [
   {name: "Kamala Khan", super_name: "Ms. Marvel"},
   {name: "Doreen Green", super_name: "Squirrel Girl" },
-  {name:"Gwen Stacy", super_name:"Spider-Gwen" },
-  {name:"Janet Van Dyne", super_name:"The Wasp" },
-  {name:"Wanda Maximoff", super_name:"Scarlet Witch" },
-  {name:"Carol Danvers", super_name:"Captain Marvel" },
-  {name:"Jean Grey", super_name:"Dark Phoenix" },
-  {name:"Ororo Munroe", super_name:"Storm" },
-  {name:"Kitty Pryde", super_name:"Shadowcat" },
-  {name:"Elektra Natchios", super_name:"Elektra" }
+  {name:"Gwen Stacy", super_name: "Spider-Gwen" },
+  {name:"Janet Van Dyne", super_name: "The Wasp" },
+  {name:"Wanda Maximoff", super_name: "Scarlet Witch" },
+  {name:"Carol Danvers", super_name: "Captain Marvel" },
+  {name:"Jean Grey", super_name: "Dark Phoenix" },
+  {name:"Ororo Munroe", super_name: "Storm" },
+  {name:"Kitty Pryde", super_name: "Shadowcat" },
+  {name:"Elektra Natchios", super_name: "Elektra" }
 ]
 
 # ---------------
@@ -34,3 +34,6 @@ heroines = heroines.map { |heroine| heroine.merge( { power_id: Power.all.sample.
 # ---------------
 
 heroines.each { |heroine| Heroine.create(heroine) }
+
+
+puts "#{Heroine.all.count} Heroines & #{Power.all.count} Powers created"
