3f3a87b9fca0b2c623ac94d8ce5d575334b72477
ch2isk4kos <ch2isk4kos@gmail.com>
2019-03-01 11:11:16 -0500

    pushed repo changes

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,23 @@
 class HeroinesController < ApplicationController
-  def index
-    @heroines = Heroine.all
-  end
+
+    def index
+        @heroines = Heroine.all
+    end
+
+    def show
+        @heroine = Heroine.find(params[:id])
+    end
+
+    def new
+        @heroine = Heroine.new
+    end
+
+
+    private
+
+    def get_params
+        params.require(:heroines).permit(:name, :super_name)
+    end
+
+
 end
