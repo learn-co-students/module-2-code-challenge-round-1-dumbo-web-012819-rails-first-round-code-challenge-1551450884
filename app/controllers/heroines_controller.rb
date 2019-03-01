a97421b160602fafca4d172d9bb18a687f68a911
github username <github email address>
2019-03-01 11:12:22 -0500

    finished code challenge, did not finish bonus

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,35 @@
 class HeroinesController < ApplicationController
+
   def index
-    @heroines = Heroine.all
+      @heroines = Heroine.all
   end
+
+   def show
+       get_heroine
+   end
+
+   def new
+       @heroine = Heroine.new
+       @powers = Power.all
+   end
+
+   def create
+       @heroine = Heroine.new(heroine_params)
+       if @heroine.valid?
+           @heroine.save
+       else
+           @powers = Power.all
+           render :new
+       end
+   end
+
+private
+
+   def get_heroine
+   @heroine = Heroine.find(params[:id])
+    end
+
+    def heroine_params
+        params.require(:heroine).permit(:name, :super_name, :power_id)
+    end
 end
