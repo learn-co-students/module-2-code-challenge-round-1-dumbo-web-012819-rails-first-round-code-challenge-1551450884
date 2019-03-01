cdc1a099b5479f23c9f2911686a23e7a9acc97e9
Krystle Justine <KrystleJustine@gmail.com>
2019-03-01 11:10:27 -0500

    Krystle's Mod 2 Code Challenge

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,35 @@
 class HeroinesController < ApplicationController
-  def index
-    @heroines = Heroine.all
-  end
+ before_action :get_heroine, only: [:show]
+
+def index
+  @heroines = Heroine.all
+end
+
+ def show
+  @powers = @heroine.power
+   # grabs instance of heroine from before action method
+ end
+
+ def new
+   @heroine = Heroine.new
+ end
+
+ def create
+   @heroine = Heroine.create(heroine_params)
+   redirect_to @heroine
+   # creates a new heroine and redirects to the show page of the NEW
+   # heroine once the user hits submit
+ end
+
+
+
+private
+def get_heroine
+  @heroine = Heroine.find(params[:id])
+end
+
+def heroine_params
+  params.require(:heroine).permit(:name, :super_name, :power_id)
+end
+
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,22 @@
 class PowersController < ApplicationController
+
+  before_action :get_powers, only: [:show]
+
+
   def index
     @powers = Power.all
+  end
+
+  def show
+    #grabs instance of power from before action metho
+  end
+
+  private
+  def get_powers
+    @power = Power.find(params[:id])
+  end
+
+  def power_params
+    params.require(:powers).permit(:name, :description)
   end
 end
